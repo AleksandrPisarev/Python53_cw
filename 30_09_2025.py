e2c7534620db1d483b81a817a2e412198cfacea1
Ivan Ivanushkin <7891235>
2025-09-30 21:02:06 +0300

    30_09_2025 число и три диапазона

---
+++ 30_09_2025.py
@@ -0,0 +1,24 @@
+number=int(input('Введите число'))
+begin_range1=int(input('Введите начало первого диапазона'))
+end_range1=int(input('Введите конец первого диапазона'))
+begin_range2=int(input('Введите начало второго диапазона'))
+end_range2=int(input('Введите конец второго диапазона'))
+begin_range3=int(input('Введите начало третьего диапазона'))
+end_range3=int(input('Введите конец третьего диапазона'))
+flag=0
+count=0
+if begin_range1<=number<=end_range1:
+    print(f'число {number} принадлежит первому диапазону')
+    flag=1
+    count+=1
+if begin_range2<=number<=end_range2:
+    print(f'число {number} принадлежит второму диапазону')
+    flag=1
+    count+=1
+if begin_range3<=number<=end_range3:
+    print(f'число {number} принадлежит третьему диапазону')
+    flag=1
+    count+=1
+if flag==0:
+    print(f'число {number} не принадлежит  ни одному диапазону')
+else: print(f'число {number} принадлежит {count} диапазонам')
